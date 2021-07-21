79b70df04a304dc8f6305eafab25800a1d79242a
balamuraleedhar <bala.pavothil@eroad.com>
2021-07-22 11:44:30 +1200

    12th run

--- inspec_tests/controls/integration_tests.rb
+++ inspec_tests/controls/integration_tests.rb
@@ -1,17 +1,3 @@
-# Test our Windows Jump Hosts Security Groups
-describe aws_security_group(group_name: input("WindowsEC2SG")) do
-  it { should exist }
-  it { should allow_in(port: 443, ipv4_range: '10.189.0.0/20') }
-  it { should allow_in(port: 80, ipv4_range: '10.189.0.0/20') }
-  it { should_not allow_in(port: 389, ipv4_range: '0.0.0.0/0') }
-end
-
-# Test our VPC Endpoint Security Group
-describe aws_security_group(group_name: input("VpcEndpointSG")) do
-  it { should exist }
-  it { should allow_in(port: 443, ipv4_range: '0.0.0.0/0') }
-end
-
 # Test our IAM Roles attached policies and availability
 describe aws_iam_role(role_name: input("CodePipelineRole")) do
   it { should exist }
