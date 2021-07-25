fdd49be0014bc03941aa501a1dc1f6f5032bd728
balamuraleedhar <bala.pavothil@eroad.com>
2021-07-26 10:58:03 +1200

    23rd run

--- inspec_tests/controls/integration_tests.rb
+++ inspec_tests/controls/integration_tests.rb
@@ -8,6 +8,6 @@ describe aws_iam_policy(policy_name: input("CodePipelineBase")) do
   it { should_not have_statement(Action: 'kms:*') }
   it { should_not have_statement(Action: 'iam:*') }
   it { should have_statement(Action: 'codepipeline:*') }
-  it { should be_attached_to_role('cassis-CodePipelineRole') }
+  it { should be_attached_to_role('Dev-IOTCORE-CodePipelineRole') }
   it { should exist }
 end
